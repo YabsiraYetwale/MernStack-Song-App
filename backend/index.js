7b9ad54a6ab0eb456398227e4b8795d888bcecc5
YabsiraYetwale <119517823+YabsiraYetwale@users.noreply.github.com>
2024-02-05 11:04:17 +0300

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,10 @@ const app = express();
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
-app.use(cors());
+// app.use(cors());
+app.use(cors({
+  origin: 'https://song-api-nine.vercel.app/songs'
+}));
 
 app.use('/songs', postRoutes);
 
