064cfcbbf48b10cfd3c28e779f663835e15fc2ba
YabsiraYetwale <119517823+YabsiraYetwale@users.noreply.github.com>
2024-03-30 10:12:34 +0300

    Update index.jsx

--- frontend/src/api/index.jsx
+++ frontend/src/api/index.jsx
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
-axios.defaults.baseURL = 'https://song-api-nine.vercel.app/'
+axios.defaults.baseURL = 'https://song-api-ac0o.onrender.com'
+// axios.defaults.baseURL = 'https://song-api-nine.vercel.app/'
 
 export const getSongsAPI = async () => axios.get('/songs')
 
